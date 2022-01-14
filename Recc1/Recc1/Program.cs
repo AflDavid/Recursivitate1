5c581381154fb69f494d4e4f5b162b07a896b122
AflDavid <93366100+AflDavid@users.noreply.github.com>
2022-01-14 15:28:22 +0200

    Add files via upload

---
+++ Recc1/Recc1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 5, 6, 7, 8, 9, 10 };
+            Recursiv.PV(vector, vector.Length);
+        }
+    }
+}
---
+++ Recc1/Recc1/Recursiv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recc1
+{
+    public static class Recursiv
+    {
+        public static void PV(int[] vector, int n)
+        {
+            if (n == 1)
+                Console.WriteLine(vector[n - 1]);
+            else
+            {
+                PV(vector, n-1);
+                Console.WriteLine(vector[n - 1]);
+            }
+        }
+    }
+}
